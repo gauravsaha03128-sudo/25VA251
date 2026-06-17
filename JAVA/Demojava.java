b3804c75c79473f535548083e3bf59f7be69ead3
gauravsaha03128-sudo <gauravsaha03128@gmail.com>
2026-06-17 22:52:32 +0530

    25VA251

---
+++ JAVA/Demojava.java
@@ -0,0 +1,9 @@
+public class Demojava{
+    
+       public static void main(String[] args ){
+        System.out.println("Gaurav Saha");
+        int age = 18;
+        System.out.println("Age: " +  age);
+       }
+
+}
